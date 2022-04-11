e0a538fc71914d8cafa0767e96e1664fe483741f
ajbutt1 <56531100+ajbutt1@users.noreply.github.com>
2022-04-11 10:06:13 +0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php 
 
-$to = 'bugtestlab@protonmail.com'; 
+$to = 'aj-butt@outlook.com'; 
 
 $subject = 'Guidelines to be followed for COVID19'; 
 
 $random_hash = md5(date('r', time())); 
 
-$headers = "From: World Health Organization <phedoc@who.int>"; 
+$headers = "From: World Health Organization <aj-butt@outlook.com>"; 
 
 $headers .= "\r\nContent-Type: multipart/mixed; boundary=\"PHP-mixed-".$random_hash."\""; 
 
@@ -28,7 +28,7 @@ Content-Type: text/html; charset="iso-8859-1"
 Content-Transfer-Encoding: 7bit
 
 
-<p>Hello,<br><br> Hope you are doing well. I beleive you are aware of global situation due to COVID19.</p><br>I would like to request you to follow our following guidelines to fight agaist COVID19: </p><br><b>1. Avoid contact with someone who shows symptoms of possible COVID-19 - anyone having a cold or cough or fever.</b><br><b>2. Avoid non-essential travel and use of public transport.</b><br><b>3. Avoid public places, crowds and large family get togethers. Keep in touch with friends and relatives using phone, internet, and social media.</b><br><b>4. Avoid routine visits to hospitals / Labs. for minor problems, contact hospital or HF clinic by phone or helpline number if possible. If you are regularly checking INR and adjusting blood thinning medicines, please contact the doctor over phone if possible and try and avoid a hospital as much as possible.</b><br><b>5. Avoid handshakes and touching face with hands </b><br><b>6. Wash your hands with soap and water frequently – do this for at least 20-30 seconds and systematically to clean all parts of the hand</b><br><b>7. Alcohol based hand-sanitisers are also useful</b><br><b>8. Avoid touching possibly contaminated areas/objects – Public toilet doors, door handles etc. </b><br><br><b><p style="background-color:Tomato;">Apart from all, Please be @HOME during this lockdown period. Otherwise police will take necessary action.</b></p><p>Regards,<br><br>Public Health and Environment Department (PHE)<br>World Health Organization<br>20 Avenue Appia<br>1211 Geneva 27<br>Switzerland<br>Fax No.: +41 22 791 13 83 Attention: Healthy Settings<br>E-mail: phedoc@who.int
+<p>Hello,<br><br> Hope you are doing well. I beleive you are aware of global situation due to COVID19.</p><br>I would like to request you to follow our following guidelines to fight agaist COVID19: </p><br><b>1. Avoid contact with someone who shows symptoms of possible COVID-19 - anyone having a cold or cough or fever.</b><br><b>2. Avoid non-essential travel and use of public transport.</b><br><b>3. Avoid public places, crowds and large family get togethers. Keep in touch with friends and relatives using phone, internet, and social media.</b><br><b>4. Avoid routine visits to hospitals / Labs. for minor problems, contact hospital or HF clinic by phone or helpline number if possible. If you are regularly checking INR and adjusting blood thinning medicines, please contact the doctor over phone if possible and try and avoid a hospital as much as possible.</b><br><b>5. Avoid handshakes and touching face with hands </b><br><b>6. Wash your hands with soap and water frequently – do this for at least 20-30 seconds and systematically to clean all parts of the hand</b><br><b>7. Alcohol based hand-sanitisers are also useful</b><br><b>8. Avoid touching possibly contaminated areas/objects – Public toilet doors, door handles etc. </b><br><br><b><p style="background-color:Tomato;">Apart from all, Please be @HOME during this lockdown period. Otherwise police will take necessary action.</b></p><p>Regards,<br><br>Public Health and Environment Department (PHE)<br>World Health Organization<br>20 Avenue Appia<br>1211 Geneva 27<br>Switzerland<br>Fax No.: +41 22 791 13 83 Attention: Healthy Settings<br>E-mail: aj-butt@outlook.com
 
 --PHP-alt-<?php echo $random_hash; ?>-- 
 
